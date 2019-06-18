bc99d4b27a6b213de3c90c810ab4cb9e1191dcbf
은지 정 <seyrahan22@gmail.com>
2019-06-18 15:30:49 +0900

    Quiz #1 마침

---
+++ SimpleQuiz/SimpleQuiz/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ######## Quiz 1번 #######
+            // 3과 5의 배수 숫자의 합 구하기
+
+            Quiz_1 quiz = new Quiz_1();
+            quiz.func1();
+        }
+    }
+}
---
+++ SimpleQuiz/SimpleQuiz/Quiz_1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleQuiz
+{
+    class Quiz_1
+    {
+        public void func1()
+        {
+            // ######## Quiz 1번 #######
+            // 3과 5의 배수 숫자의 합 구하기
+
+            int sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sum += i;
+                }
+                else if (i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine($"1~1000 사이의 3과 5의 배수 합 : {sum}");
+        }
+    }
+}
